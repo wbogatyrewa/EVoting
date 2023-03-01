c2ed65e29e610b0ea9867f6ea82425da770ae069
wbogatyrewa <veronika01-01@mail.ru>
2023-03-01 21:42:18 +0500

    add eslint default export rule forbidden

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,50 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    "prettier/prettier": "error"
-  }
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "ExportDefaultDeclaration",
+        message: "Restricted default export, prefer named exports!",
+      },
+    ],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    overrides: [
+      {
+        files: ["webpack.config.js"],
+        rules: {
+          "@typescript-eslint/no-var-requires": ["off"],
+        },
+      },
+      {
+        files: ["*.stories.tsx"],
+        rules: {
+          "no-restricted-syntax": ["off"],
+        },
+      },
+    ],
+  },
 };
