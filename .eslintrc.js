5b9dfea528112f37d663f4589ac12079f93890f4
wbogatyrewa <veronika01-01@mail.ru>
2023-03-01 21:36:47 +0500

    add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    "prettier/prettier": "error",
-  },
+    "prettier/prettier": "error"
+  }
 };
