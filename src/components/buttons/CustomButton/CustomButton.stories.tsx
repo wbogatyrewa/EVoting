43fae36a66bf5b661dc88fa70b5ce198305f4ca0
wbogatyrewa <veronika01-01@mail.ru>
2023-03-03 00:19:38 +0500

    add button

---
+++ src/components/buttons/CustomButton/CustomButton.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { CustomButton } from ".";
+
+export default {
+  title: "Buttons",
+} as ComponentMeta<typeof CustomButton>;
+
+const Template: ComponentStory<typeof CustomButton> = (args) => 
+  <CustomButton {...args}>Проголосовать</CustomButton>
+
+export const Primary = Template.bind({});
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  color: "secondary"
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true
+};
---
+++ src/components/buttons/CustomButton/CustomButton.tsx
@@ -0,0 +1,41 @@
+import { Button, ButtonProps } from "@mui/material";
+import React, { FC } from "react";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#6750A4',
+      light: '#735EAB',
+      dark: '#7965AF'
+    },
+    secondary: {
+      main: '#E8DEF8',
+      light: '#D8CEE8',
+      dark: '#D0C6DF',
+      contrastText: '#1D192B'
+    },
+  },
+  typography: {
+    button: {
+      textTransform: "initial"
+    }
+  },
+});
+
+export const CustomButton: FC<ButtonProps> = ({ onClick, children, ...props }: ButtonProps) => {
+  return (
+  <ThemeProvider theme={theme}>
+    <Button 
+      onClick={onClick}
+      disableElevation
+      disableFocusRipple
+      disableRipple
+      variant="contained"
+      sx={{ borderRadius: '100px' }}
+      {...props}>
+        {children}
+    </Button>
+  </ThemeProvider>
+  );
+};
