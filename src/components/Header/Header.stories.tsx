dad20b203562fdf33541eb7a06c086243dab3dfa
wbogatyrewa <veronika01-01@mail.ru>
2023-03-03 22:20:57 +0500

    add Header

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Header } from './Header';
+
+export default {
+  title: "Header",
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => 
+  <Header {...args}></Header>
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  childrenButton: "Авторизоваться"
+}
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  account: "0x543EbD3F56B2b9848b246C689E0302dC06CcFa48"
+}
+
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,42 @@
 import React, { FC } from "react";
+import styled from "@emotion/styled";
+import { Logo } from "./Logo";
+import { Box } from "@mui/material";
+import { CustomButton } from "../buttons/CustomButton";
+import { MetaMaskIcon } from "../icons";
+import { UserAccount } from "./UserAccount";
 
-export const Header: FC<unknown> = () => {
+export interface Props {
+  account?: string;
+  onLogin?: () => void;
+  childrenButton?: React.ReactNode;
+}
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 48px;
+`;
+
+export const Header: FC<Props> = ({ account, onLogin, childrenButton }: Props) => {
   return (
-    <header></header>
+    <HeaderWrapper>
+      <Box>
+        <Logo />
+      </Box>
+      <Box>
+        {
+          account ?
+          <UserAccount icon={<MetaMaskIcon />} account={account} />
+          : 
+          <CustomButton
+            onClick={onLogin}
+            startIcon={<MetaMaskIcon />}>
+              {childrenButton}
+        </CustomButton>
+        }
+      </Box>
+    </HeaderWrapper>
   );
 };
