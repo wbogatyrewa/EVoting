d3ca2dcccf7da365555d8116531c03cd4611feb7
wbogatyrewa <veronika01-01@mail.ru>
2023-03-23 11:45:17 +0500

    add more props for about voting page

--- src/components/pages/AboutVotingPage/AboutVotingPage.stories.tsx
+++ src/components/pages/AboutVotingPage/AboutVotingPage.stories.tsx
@@ -11,25 +11,42 @@ export default {
 
 const Template: ComponentStory<typeof AboutVotingPage> = (args) =>
 <>
-  <Header account="0x543EbD3F56B2b9848b246C689E0302dC06CcFa48" />
+  <Header account={args.account} />
   <Container maxWidth="lg" sx={{
         marginTop: "32px",
         "@media (min-width: 1200px)": {
           maxWidth: '1128px'
         }
       }}>
-    <AboutVotingPage 
-      title={'Чизбургер или чикенбургер?'} 
-      startDateTime={new Date('March 1, 2023 03:24:00')} 
-      endDateTime={new Date('March 31, 2023 03:24:00')} 
-      link={'https://etherscan.io/'} 
-      answers={[
-        { label: 'Чизбургер' }, 
-        { label: 'Чикенбургер' }
-      ]}  />
+    <AboutVotingPage {...args}/>
   </Container>
 </>
 
+export const AboutVotingActive = Template.bind({});
+AboutVotingActive.args = {
+  title: "AppLe или AnDrOiD?",
+  startDateTime: new Date('March 1, 2023 03:24:00'),
+  endDateTime: new Date('March 31, 2023 03:24:00'),
+  answers: [
+    { label: "Apple" },
+    { label: "Android" }
+  ],
+  linkSmartContract: "https://etherscan.io/",
+  account: "0x543EbD3F56B2b9848b246C689E0302dC06CcFa48"
+};
 
-export const AboutVoting = Template.bind({});
+export const AboutVotingActiveVoted = Template.bind({});
+AboutVotingActiveVoted.args = {
+  title: "AppLe или AnDrOiD?",
+  startDateTime: new Date('March 1, 2023 03:24:00'),
+  endDateTime: new Date('March 31, 2023 03:24:00'),
+  answers: [
+    { label: "Apple" },
+    { label: "Android" }
+  ],
+  linkSmartContract: "https://etherscan.io/",
+  account: "0x543EbD3F56B2b9848b246C689E0302dC06CcFa48",
+  linkVote: "https://etherscan.io/",
+  isVoted: true,
+};
 
--- src/components/pages/AboutVotingPage/AboutVotingPage.tsx
+++ src/components/pages/AboutVotingPage/AboutVotingPage.tsx
@@ -1,7 +1,8 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import React, { FC } from "react";
 import { AboutVotingCard } from "../../cards/AboutVotingCard";
 import { RadioList } from "../../RadioList";
+import { TransactionLink } from "../../TransactionLink";
 import { Answer } from "../../Types";
 import { Page } from "../Page";
 
@@ -9,29 +10,45 @@ interface Props {
   title: string;
   startDateTime: Date;
   endDateTime: Date;
-  link: string;
+  linkSmartContract: string;
   answers: Array<Answer>;
   account?: string;
+  linkVote?: string;
+  isVoted: boolean;
 }
 
-export const AboutVotingPage: FC<Props> = ({title, startDateTime, endDateTime, link, answers, account}: Props) => {
+export const AboutVotingPage: FC<Props> = ({title, startDateTime = new Date("March 1, 2023 03:24:00"), 
+          endDateTime = new Date("March 31, 2023 03:24:00"), linkSmartContract, answers, 
+          account, linkVote = "", isVoted = false}: Props) => {
   const handleClickClose = () => {};
 
   return (
-    <Page title={title} closed handleClose={handleClickClose} voted={account ? true : false}>
+    <Page title={title} closed handleClose={handleClickClose} voted={account ? true : false} disabledBtn={isVoted}>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
+        <Grid item xs={4}>
           <Box mb={2}>
             <AboutVotingCard 
               startDateTime={startDateTime} 
               endDateTime={endDateTime} 
-              link={link}
+              link={linkSmartContract}
             />
           </Box>
           <Box>
-            <RadioList label="Варианты ответов" radioList={answers} disabled={account ? false : true} />
+            <RadioList label="Варианты ответов" radioList={answers} disabled={isVoted ? true : account ? false : true} />
           </Box>
         </Grid>
+        <Grid item xs={4}></Grid>
+        {
+          isVoted ? 
+          <Grid item xs={4}>
+            <Box display="flex" gap="3px" mb="0.35em">
+              <TransactionLink link={linkVote}>Транзакция</TransactionLink>
+              <Typography variant="body2" color="text.primary">вашего голоса записана в блокчейн</Typography>
+            </Box>
+          </Grid>
+          : null
+        }
+        
       </Grid>
     </Page>
   );
