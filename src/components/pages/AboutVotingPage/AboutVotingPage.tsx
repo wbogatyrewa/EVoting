bc4dd48370d047d44009f55f621da6a3f0f23920
wbogatyrewa <veronika01-01@mail.ru>
2023-03-27 01:44:24 +0500

    get address from params

--- src/components/pages/AboutVotingPage/AboutVotingPage.tsx
+++ src/components/pages/AboutVotingPage/AboutVotingPage.tsx
@@ -1,5 +1,6 @@
 import { Box, Grid, Typography } from "@mui/material";
 import React, { FC } from "react";
+import { useParams } from "react-router-dom";
 import { AboutVotingCard } from "../../cards/AboutVotingCard";
 import { RadioList } from "../../RadioList";
 import { TransactionLink } from "../../TransactionLink";
@@ -18,8 +19,13 @@ interface Props {
 }
 
 export const AboutVotingPage: FC<Props> = ({title, startDateTime = new Date("March 1, 2023 03:24:00"), 
-          endDateTime = new Date("March 31, 2023 03:24:00"), linkSmartContract = "", answers = [], 
-          account, linkVote = "", isVoted = false}: Props) => {
+  endDateTime = new Date("March 31, 2023 03:24:00"), linkSmartContract = "", answers = [], 
+  account, linkVote = "", isVoted = false}: Props) => {
+    
+  const params = useParams();
+  const address = params.votingAddress;
+
+  // получить данные со смарта
   const now = new Date().getTime();
   const status = now >= startDateTime.getTime() ? now <= endDateTime.getTime() ? 
     Status.Active : Status.Finished : Status.Before;
