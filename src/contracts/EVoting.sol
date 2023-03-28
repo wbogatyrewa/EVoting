6f814aa244cd980f41fb05eba33d632d8a621e38
wbogatyrewa <veronika01-01@mail.ru>
2023-03-28 22:12:43 +0500

    add methods to EVoting smart contract

--- src/contracts/EVoting.sol
+++ src/contracts/EVoting.sol
@@ -10,10 +10,13 @@ contract EVoting {
     address chairperson;
     EVotingToken token = new EVotingToken();
 
+    address[] votersAddresses;
     mapping (address => bool) voters; // {address of voter => voted}
     mapping (address => bool) votersExist;
     mapping (address => address) votesFor;
 
+    address[] proposalsAddresses;
+    string[] proposalsNames;
     mapping (address => string) proposals; // {address of proposal => name of proposal}
     mapping (address => bool) proposalsExist;
 
@@ -26,6 +29,9 @@ contract EVoting {
         startTime = _startTime;
         endTime = _endTime;
 
+        votersAddresses = _voters;
+        proposalsAddresses = _proposalsAddr;
+        proposalsNames = _proposalsNames;
         addVoters(_voters);
         addProposals(_proposalsAddr, _proposalsNames);
     }
@@ -82,6 +88,30 @@ contract EVoting {
     function voteFor(address voter) view public returns (address) {
         require(votersExist[voter], "Voter doesn't exist.");
         return votesFor[voter];
+    }
+
+        function getName() view public returns(string memory) {
+        return name;
+    }
+
+    function getStartTime() view public returns(uint) {
+        return startTime;
+    }
+
+    function getEndTime() view public returns(uint) {
+        return endTime;
+    }
+
+    function getVoters() view public returns(address[] memory) {
+        return votersAddresses;
+    }
+
+    function getPropposalsAddresses() view public returns(address[] memory) {
+        return proposalsAddresses;
+    }
+
+    function getPropposalsNames() view public returns(string[] memory) {
+        return proposalsNames;
     }
 
     function nameOfProposal(address proposal) view public returns (string memory) {
