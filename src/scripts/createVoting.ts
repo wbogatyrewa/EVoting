5b19c9e6c629546259d8771ab8df11caba91c4ac
wbogatyrewa <veronika01-01@mail.ru>
2023-04-13 01:44:57 +0500

    add create voting

---
+++ src/scripts/createVoting.ts
@@ -0,0 +1,34 @@
+import { Dayjs } from "dayjs";
+
+export interface Props {
+  name: string;
+  startDateTime: Dayjs | null;
+  endDateTime: Dayjs | null;
+  voters: string[];
+  proposalsNames: string[];
+}
+
+export const createVoting = async ({ name, startDateTime, endDateTime, voters, proposalsNames }: Props ) => {
+  let address = "";
+  const url = "http://localhost:8000/create-voting";
+  
+  const voting = {
+    name: name,
+    startDateTime: startDateTime?.unix(),
+    endDateTime: endDateTime?.unix(),
+    voters: voters,
+    proposalsNames: proposalsNames
+  };
+  let response = await fetch(url, {
+    method: "POST", 
+    headers: { 
+      "Accept": "application/json", 
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(voting)
+  });
+  if (response.ok) {
+    address = await response.json();
+  }
+  return address;
+};
